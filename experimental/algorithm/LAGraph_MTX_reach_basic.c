e98e0ca41d8533a6654633daaa07b13ed34b235d
Homka122 <kombaev02@gmail.com>
2024-11-10 10:08:02 +0300

    Feat: add processing rules

--- experimental/algorithm/LAGraph_MTX_reach_basic.c
+++ experimental/algorithm/LAGraph_MTX_reach_basic.c
@@ -34,44 +34,68 @@ GrB_Info LAGraph_MTX_reach_basic(GrB_Matrix *output, GrB_Matrix *adj_matrices,
         GRB_TRY(GrB_Matrix_new(&T[i], GrB_BOOL, n, n));
     }
 
-    // Rule [Variable -> term]
+    // Arrays for processing rules
+    size_t eps_rules[rules_count], eps_rules_count = 0;   // [Variable -> eps]
+    size_t term_rules[rules_count], term_rules_count = 0; // [Variable -> term]
+    size_t bin_rules[rules_count], bin_rules_count = 0;   // [Variable -> AB]
+
+    // Process rules
     for (size_t i = 0; i < rules_count; i++) {
         LAGraph_rule_WCNF rule = rules[i];
-        // Only [Variable -> term] type of rule accepted
-        if (rule.prod_A == -1 || rule.prod_B != -1)
+
+        // [Variable -> eps]
+        if (rule.prod_A == -1 && rule.prod_B == -1) {
+            eps_rules[eps_rules_count++] = i;
             continue;
+        }
+
+        // [Variable -> term]
+        if (rule.prod_A != -1 && rule.prod_B == -1) {
+            term_rules[term_rules_count++] = i;
+            continue;
+        }
+
+        // [Variable -> A B]
+        if (rule.prod_A != -1 && rule.prod_B != -1) {
+            bin_rules[bin_rules_count++] = i;
+            continue;
+        }
+
+        // [Variable -> _ B]
+        LG_ASSERT_MSG(true, GrB_INVALID_VALUE,
+                      "Rule with index %d is invalid. [Variable -> _ B] type of rule "
+                      "don't accepted.");
+    }
+
+    // Rule [Variable -> term]
+    for (size_t i = 0; i < term_rules_count; i++) {
+        LAGraph_rule_WCNF term_rule = rules[term_rules[i]];
 
         GrB_Index nnz;
-        GrB_Matrix_nvals(&nnz, adj_matrices[rule.prod_A]);
+        GrB_Matrix_nvals(&nnz, adj_matrices[term_rule.prod_A]);
 
         GrB_Index row[nnz], col[nnz];
         bool values[nnz];
 
-        GrB_Matrix_extractTuples(row, col, values, &nnz, adj_matrices[rule.prod_A]);
-
+        GrB_Matrix_extractTuples(row, col, values, &nnz, adj_matrices[term_rule.prod_A]);
         for (size_t j = 0; j < nnz; j++) {
 #ifdef DEBUG
             printf("[TERM] SET ELEMENT [TRUE], NONTERM: %d, ROW: %ld, COL: %ld\n",
                    nonterm, row[j], col[j]);
 #endif
-            GrB_Matrix_setElement(T[rule.nonterm], 1, row[j], col[j]);
+            GrB_Matrix_setElement(T[term_rule.nonterm], 1, row[j], col[j]);
         }
     }
 
     // Rule [Variable -> eps]
-    for (size_t i = 0; i < rules_count; i++) {
-        LAGraph_rule_WCNF rule = rules[i];
-
-        // Only [Variable -> eps] type of rule accepted
-        if (rule.prod_A != -1) {
-            continue;
-        }
+    for (size_t i = 0; i < eps_rules_count; i++) {
+        LAGraph_rule_WCNF eps_rule = rules[eps_rules[i]];
 
         for (size_t j = 0; j < n; j++) {
 #ifdef DEBUG
             printf("[EPS] SET ELEMENT [TRUE], NONTERM: %d, INDEX: %ld\n", nonterm, j);
 #endif
-            GrB_Matrix_setElement(T[rule.nonterm], true, j, j);
+            GrB_Matrix_setElement(T[eps_rule.nonterm], true, j, j);
         }
     }
 
@@ -80,27 +104,23 @@ GrB_Info LAGraph_MTX_reach_basic(GrB_Matrix *output, GrB_Matrix *adj_matrices,
     bool changed = true;
     while (changed) {
         changed = false;
-        for (size_t i = 0; i < rules_count; i++) {
-            LAGraph_rule_WCNF rule = rules[i];
+        for (size_t i = 0; i < bin_rules_count; i++) {
+            LAGraph_rule_WCNF bin_rule = rules[bin_rules[i]];
 
-            // Only [Variable -> Variable1 Variable2] type of rule accepted
-            if (rule.prod_A == -1 || rule.prod_B == -1) {
-                continue;
-            }
-
-            GRB_TRY(GrB_mxm(T[rule.nonterm], GrB_NULL, GxB_LOR_BOOL, GxB_ANY_PAIR_BOOL,
-                            T[rule.prod_A], T[rule.prod_B], GrB_NULL));
+            GRB_TRY(GrB_mxm(T[bin_rule.nonterm], GrB_NULL, GxB_LOR_BOOL,
+                            GxB_ANY_PAIR_BOOL, T[bin_rule.prod_A], T[bin_rule.prod_B],
+                            GrB_NULL));
 
             GrB_Index new_nnz;
-            GRB_TRY(GrB_Matrix_nvals(&new_nnz, T[rule.nonterm]));
+            GRB_TRY(GrB_Matrix_nvals(&new_nnz, T[bin_rule.nonterm]));
 
-            changed = changed | (nnz[rule.nonterm] != new_nnz);
+            changed = changed | (nnz[bin_rule.nonterm] != new_nnz);
 #ifdef DEBUG
             printf("[TERM1 TERM2] MULTIPLY, S: %d, A: %d, B: %d, "
                    "I: %ld\n",
                    nonterm, nonterm_A, nonterm_B, i);
 #endif
-            nnz[rule.nonterm] = new_nnz;
+            nnz[bin_rule.nonterm] = new_nnz;
         }
     }
 
