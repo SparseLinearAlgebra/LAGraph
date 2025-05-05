8b16e59d9799b94f9b92528af8e14d195d32b43a
Homka122 <kombaev02@gmail.com>
2025-05-05 23:28:03 +0300

    Feat: add algorithm with optimization to header

--- include/LAGraphX.h
+++ include/LAGraphX.h
@@ -1096,6 +1096,33 @@ GrB_Info LAGraph_CFL_reachability
     char *msg                       // Message string for error reporting.
 ) ;
 
+GrB_Info LAGraph_CFL_reachability_adv
+(
+    // Output
+    GrB_Matrix *outputs, // Array of matrices containing results.
+                         // The size of the array must be equal to nonterms_count.
+                         //
+                         // outputs[k]: (i, j) = true if and only if there is a path
+                         // from node i to node j whose edge labels form a word
+                         // derivable from the non-terminal 'k' of the specified CFG.
+
+    // Input
+    const GrB_Matrix *adj_matrices, // Array of adjacency matrices representing the graph.
+                                    // The length of this array is equal to the count of
+                                    // terminals (terms_count).
+                                    //
+                                    // adj_matrices[t]: (i, j) == 1 if and only if there
+                                    // is an edge between nodes i and j with the label of
+                                    // the terminal corresponding to index 't' (where t is
+                                    // in the range [0, terms_count - 1]).
+
+    int32_t terms_count,    // The total number of terminal symbols in the CFG.
+    int32_t nonterms_count, // The total number of non-terminal symbols in the CFG.
+    const LAGraph_rule_WCNF *rules, // The rules of the CFG.
+    size_t rules_count,             // The total number of rules in the CFG.
+    char *msg                       // Message string for error reporting.
+);
+
 //------------------------------------------------------------------------------
 // a simple example of an algorithm
 //------------------------------------------------------------------------------
