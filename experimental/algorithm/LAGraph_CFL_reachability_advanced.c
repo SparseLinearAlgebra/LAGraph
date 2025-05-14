fe16527a81b834cb867a0fd3e6ed33475d8a2c40
Homka122 <kombaev02@gmail.com>
2025-05-14 09:12:07 +0300

    Fix: change NULL to GrB_NULL

--- experimental/algorithm/LAGraph_CFL_reachability_advanced.c
+++ experimental/algorithm/LAGraph_CFL_reachability_advanced.c
@@ -269,7 +269,7 @@ GrB_Info matrix_dup_empty(Matrix *output, Matrix *input) {
 }
 
 GrB_Info matrix_mxm(Matrix *output, Matrix *first, Matrix *second, bool accum) {
-    GrB_mxm(output->base, GrB_NULL, accum ? GxB_ANY_BOOL : NULL, GxB_ANY_PAIR_BOOL,
+    GrB_mxm(output->base, GrB_NULL, accum ? GxB_ANY_BOOL : GrB_NULL, GxB_ANY_PAIR_BOOL,
             first->base, second->base, GrB_NULL);
     IS_ISO(output->base, "MXM output");
 }
