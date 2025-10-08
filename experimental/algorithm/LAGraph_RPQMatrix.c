7fc0a7004d53442563f782055eecc20133de88d9
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2025-10-08 21:29:51 +0300

    fix: correct non null assert
    Fix the typo in non null assertion. we
    should check res_mat ofc.
    
    Signed-off-by: Rodion Suvorov <rodion.suvorov.94@mail.ru>

--- experimental/algorithm/LAGraph_RPQMatrix.c
+++ experimental/algorithm/LAGraph_RPQMatrix.c
@@ -213,8 +213,8 @@ static GrB_Info LAGraph_RPQMatrixLor(RPQMatrixPlan *plan, char *msg)
     OK(LAGraph_RPQMatrix_solver(lhs, msg)) ;
     OK(LAGraph_RPQMatrix_solver(rhs, msg)) ;
 
-    LG_ASSERT(rhs->mat != NULL, GrB_NULL_POINTER) ;
-    LG_ASSERT(lhs->mat != NULL, GrB_NULL_POINTER) ;
+    LG_ASSERT(rhs->res_mat != NULL, GrB_NULL_POINTER) ;
+    LG_ASSERT(lhs->res_mat != NULL, GrB_NULL_POINTER) ;
 
     GrB_Matrix lhs_mat = lhs->res_mat ;
     GrB_Matrix rhs_mat = rhs->res_mat ;
