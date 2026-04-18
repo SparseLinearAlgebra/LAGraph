d97c67bdcca9216d8eb6459fe10f39d8693a02f8
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2026-04-18 16:05:01 +0300

    ref: delete useless commented code in ms adv

--- experimental/algorithm/LAGraph_CFL_reachability_multsrc_advanced.c
+++ experimental/algorithm/LAGraph_CFL_reachability_multsrc_advanced.c
@@ -649,7 +649,6 @@ GrB_Info LAGraph_CFL_reachability_multsrc_adv
     TRY(get_new_rules(rules, rules_count, to_new_symbols_map, new_symbols_amount,
                       &new_rules, &new_rules_count, msg, opt_mask));
 
-
     // Arrays for processing rules
     size_t eps_rules[new_rules_count], eps_rules_count = 0;   // [Variable -> eps]
     size_t term_rules[new_rules_count], term_rules_count = 0; // [Variable -> term]
@@ -667,7 +666,6 @@ GrB_Info LAGraph_CFL_reachability_multsrc_adv
     rule_error_s invalid_err = {0};
 
     for (size_t i = 0; i < rules_count; i++) {
-        // LAGraph_rule_WCNF rule = rules[i];
         LAGraph_rule_EWCNF rule = new_rules[i];
 
         bool is_rule_eps = rule.prod_A == -1 && rule.prod_B == -1;
@@ -758,14 +756,10 @@ GrB_Info LAGraph_CFL_reachability_multsrc_adv
 
         TRY(GrB_Matrix_dup(&new_adj_matrix, new_adj_matrices[i]));
         if (opt_mask & OPT_LAZY) {
-            // TRY(CFL_matrix_create_lazy(&T[i], n, n));
             CFL_matrix_from_base_lazy(&T[i], new_adj_matrix);
         } else {
-            // TRY(CFL_matrix_create(&T[i], n, n));
             CFL_matrix_from_base(&T[i], new_adj_matrix);
         }
-
-        // TRY(CFL_matrix_create(&dT[i], n, n));
 
         TRY(CFL_matrix_create(&TSrc[i], n, n));
     }
@@ -774,7 +768,6 @@ GrB_Info LAGraph_CFL_reachability_multsrc_adv
         GrB_Matrix_setElement(TSrc[0]->base, true, src[i], src[i]);
     }
     TRY(CFL_matrix_update(TSrc[0]));
-    // TRY(CFL_matrix_from_base(&TSrc[0], TSrc[0]->base));
 
     TRY(CFL_matrix_create(&MSrc, n, n));
     TRY(CFL_matrix_create(&M1, n, n));
@@ -787,7 +780,6 @@ GrB_Info LAGraph_CFL_reachability_multsrc_adv
 
     // Rule [Variable -> term]
     for (size_t i = 0; i < term_rules_count; i++) {
-        // LAGraph_rule_WCNF term_rule = rules[term_rules[i]];
         LAGraph_rule_EWCNF term_rule = new_rules[term_rules[i]];
 
         // I'd like to get rid of double operation
@@ -797,7 +789,6 @@ GrB_Info LAGraph_CFL_reachability_multsrc_adv
 
     // Rule [Variable -> eps]
     for (size_t i = 0; i < eps_rules_count; i++) {
-        // LAGraph_rule_WCNF eps_rule = rules[eps_rules[i]];
         LAGraph_rule_EWCNF eps_rule = new_rules[eps_rules[i]];
 
         GRB_TRY(CFL_wise(T[eps_rule.nonterm], T[eps_rule.nonterm], iden, true, opt_mask));
@@ -809,7 +800,6 @@ GrB_Info LAGraph_CFL_reachability_multsrc_adv
     while (changed) {
         changed = false;
         for (size_t i = 0; i < bin_rules_count; i++) {
-            // LAGraph_rule_WCNF bin_rule = rules[bin_rules[i]];
             LAGraph_rule_EWCNF bin_rule = new_rules[bin_rules[i]];
 
             TRY(CFL_mxm(M1, TSrc[bin_rule.nonterm], T[bin_rule.prod_A], false, false, opt_mask));
@@ -876,8 +866,6 @@ GrB_Info LAGraph_CFL_reachability_multsrc_adv
         TRY(GrB_Matrix_dup(output, res->base));
         CFL_matrix_free(&res);
     }
-
-    // TRY(split_CFL_matrix(output, MSrc, opt_mask));
 
     LG_FREE_ALL;
     return GrB_SUCCESS;
